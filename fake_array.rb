e530a1b92d715572f9a967cb564844ab63ceb57f
Blake Johnson <blake41@gmail.com>
2014-06-02 17:18:55 -0400

    Initial Commit

--- fake_array.rb
+++ fake_array.rb
@@ -1,5 +1,23 @@
 class FakeArray
 
-  # you'll need a splat in this class somewhere
+  attr_accessor :items
+
+  def initialize(*args)
+    @items = args
+  end
+
+  def [](index)
+    self.items[index]
+  end
+
+  def first
+    self.items[0]
+  end
+
+  def each
+    self.items.each do |element|
+      yield(element)
+    end
+  end
 
 end
